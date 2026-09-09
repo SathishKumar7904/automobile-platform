5b95b8a6622c8f7df3c1aa834c394cb8bdbd4be3
Sathish K <Sathish.Kumar@bmwtechworks.in>
2026-09-09 16:31:20 +0530

    test: add enquiry service tests

---
+++ services/enquiry-service/src/test/java/com/automobile/enquiry/EnquiryServiceApplicationTests.java
@@ -0,0 +1,5 @@
+package com.automobile.enquiry;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class EnquiryServiceApplicationTests { @Test void contextLoads() {} }
