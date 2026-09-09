3748d17bfe95b091baaa7ac2d7f1c26e9cf56a37
Sathish Kumar <sathish.kumar@bmwtechworks.in>
2026-09-09 19:36:20 +0530

    fix: configure enquiry service REST clients

--- services/enquiry-service/src/main/java/com/automobile/enquiry/EnquiryServiceApplication.java
+++ services/enquiry-service/src/main/java/com/automobile/enquiry/EnquiryServiceApplication.java
@@ -2,9 +2,12 @@ package com.automobile.enquiry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EnquiryServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EnquiryServiceApplication.class, args);
     }
